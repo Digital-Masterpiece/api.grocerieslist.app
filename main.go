4da53ba18dd34d9868d37a8b3bfc99bb7726433e
Nick Frost <nickfrost.us@gmail.com>
2021-11-09 17:04:59 -0500

    starting on web server processing

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bytes"
 	"fmt"
+	"io"
 	"io/ioutil"
 	"log"
 	"net/http"
@@ -13,7 +14,36 @@ import (
 // http://localhost:8080?import=eyJpZCI6Ijk5ZDg4YWExLTQxZWItNDM4MS1hZGYwLTUzY2VmYzY2NTkxNyIsIm5hbWUiOiJTaG9ydCBMaXN0IiwiaXRlbXMiOlt7ImlkIjoiZmRkMjAyOWEtNWY4YS00NWIxLWEwYjQtNmMwNTFhOWNmODU5IiwibmFtZSI6IkJyZWFkIiwicXVhbnRpdHkiOjEsImNoZWNrZWQiOnRydWUsImNyZWF0ZWQiOjE2MzY0NzYxNDQwNTcsInVwZGF0ZWQiOjE2MzY0ODQxNjIxMDcsImRlbGV0ZWQiOm51bGx9LHsiaWQiOiI3YTM0NDQ3ZS0xZWM3LTRjOTYtYjM5OS05NDM1M2ZjNWU0NmIiLCJuYW1lIjoiRWdncyIsInF1YW50aXR5IjoxLCJjaGVja2VkIjpmYWxzZSwiY3JlYXRlZCI6MTYzNjQ3NjE0NDg1NywidXBkYXRlZCI6MTYzNjQ3NjE0NDg1NywiZGVsZXRlZCI6bnVsbH0seyJpZCI6IjZiYmY4ZGM5LTExN2UtNDAyNC1hZTQ1LTVhMDA4Y2NkMmRiOCIsIm5hbWUiOiJNaWxrIiwicXVhbnRpdHkiOjEsImNoZWNrZWQiOmZhbHNlLCJjcmVhdGVkIjoxNjM2NDgzNDYyMDcxLCJ1cGRhdGVkIjoxNjM2NDgzNDYyMDcxLCJkZWxldGVkIjpudWxsfV19
 
 func main() {
-	jsonStr := []byte(`{"domain": "s.grocerieslist.app", "target": "http://localhost:8080?import=eyJpZCI6Ijk5ZDg4YWExLTQxZWItNDM4MS1hZGYwLTUzY2VmYzY2NTkxNyIsIm5hbWUiOiJTaG9ydCBMaXN0IiwiaXRlbXMiOlt7ImlkIjoiZmRkMjAyOWEtNWY4YS00NWIxLWEwYjQtNmMwNTFhOWNmODU5IiwibmFtZSI6IkJyZWFkIiwicXVhbnRpdHkiOjEsImNoZWNrZWQiOnRydWUsImNyZWF0ZWQiOjE2MzY0NzYxNDQwNTcsInVwZGF0ZWQiOjE2MzY0ODQxNjIxMDcsImRlbGV0ZWQiOm51bGx9LHsiaWQiOiI3YTM0NDQ3ZS0xZWM3LTRjOTYtYjM5OS05NDM1M2ZjNWU0NmIiLCJuYW1lIjoiRWdncyIsInF1YW50aXR5IjoxLCJjaGVja2VkIjpmYWxzZSwiY3JlYXRlZCI6MTYzNjQ3NjE0NDg1NywidXBkYXRlZCI6MTYzNjQ3NjE0NDg1NywiZGVsZXRlZCI6bnVsbH0seyJpZCI6IjZiYmY4ZGM5LTExN2UtNDAyNC1hZTQ1LTVhMDA4Y2NkMmRiOCIsIm5hbWUiOiJNaWxrIiwicXVhbnRpdHkiOjEsImNoZWNrZWQiOmZhbHNlLCJjcmVhdGVkIjoxNjM2NDgzNDYyMDcxLCJ1cGRhdGVkIjoxNjM2NDgzNDYyMDcxLCJkZWxldGVkIjpudWxsfV19"}`)
+	http.HandleFunc("/", endpoint)
+	fmt.Println("Starting endpoint for grocerieslist.app short urls.")
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func endpoint(rw http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		http.Error(rw, "404 Not Found", http.StatusNotFound)
+	}
+
+	switch r.Method {
+	case "GET":
+		fmt.Println("GET request received.")
+		if _, err := io.WriteString(rw, "Send a POST request with the target URL you want shortened."); err != nil {
+			log.Fatal(err)
+		}
+	case "POST":
+		fmt.Println("POST request received.")
+		if err := r.ParseForm(); err != nil {
+			if _, pErr := io.WriteString(rw, "There was an error parsing your request."); pErr != nil {
+				log.Fatal(pErr)
+			}
+		}
+	}
+}
+
+func createKuttLink(t string) {
+	jsonStr := []byte(`{"domain": "s.grocerieslist.app", "target": "` + t + `}`)
 
 	client := http.Client{}
 	req, err := http.NewRequest("POST", "https://kutt.it/api/v2/links", bytes.NewBuffer(jsonStr))
